76147f9e8dedcfa7a29609c2b0bba8e93e6b7d56
zhaohx <zhaohx0409@163.com>
2024-02-28 09:34:14 +0800

    fix:由于活动模版太大所以删除部分图片 缩小文件大小

--- template-react-promotion/src/pages/images.ts
+++ template-react-promotion/src/pages/images.ts
@@ -7,40 +7,4 @@ export default [
         vendorName: '上海麦克林生化科技股份有限公司',
         only: 'lonely'
     },
-    {
-        icon: imageList['02-毕得'],
-        only: 'lonely',
-        vendorID: 699,
-        vendorName: '上海毕得医药科技股份有限公司'
-    },
-    {
-        icon: imageList['03-阿拉丁'],
-        only: 'lonely',
-        vendorID: 4332,
-        vendorName: '上海阿拉丁生化科技股份有限公司'
-    },
-    {
-        icon: imageList['08-北京百灵威'],
-        only: 'lonely',
-        vendorID: 15352,
-        vendorName: '北京百灵威科技有限公司'
-    },
-    {
-        icon: imageList['09-陶术'],
-        only: 'lonely',
-        vendorID: 4581,
-        vendorName: '上海陶术生物科技有限公司'
-    },
-    {
-        icon: imageList['10-安谱-满赠'],
-        only: 'lonely',
-        vendorID: 18433,
-        vendorName: '上海安谱实验科技股份有限公司'
-    },
-    {
-        icon: imageList['11-笛柏'],
-        only: 'lonely',
-        vendorID: 717,
-        vendorName: '上海笛柏生物科技有限公司'
-    }
 ]
